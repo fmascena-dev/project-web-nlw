bdffdf8298b4f9f6182ccd19b1ec0ff7d97dc4ac
felipe mascena <felipe.mascena.dev@gmail.com>
2025-02-18 03:16:27 -0300

    docs: Change in structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import Button from "./components/button";
+import { ArrowRight, Copy } from 'lucide-react';
+import Button from './components/button';
+import IconButton from './components/icon-button';
+import Input from './components/input';
 
 export default function Home() {
   return (
     <>
-    <Button />
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
+
+      <div>
+        <Input type="email" />
+      </div>
     </>
-  
-);
+  );
 }
