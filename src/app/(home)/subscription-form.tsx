a40291fd6396c9cfa98a2d078b5e57ba1e1fc49f
felipe mascena <felipe.mascena.dev@gmail.com>
2025-02-19 20:01:53 -0300

    feat: Implementation of React-Hook-Form and lib Zod, name and email validation

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -1,62 +1,85 @@
-import { User, Mail, ArrowRight } from "lucide-react";
-import { register } from "module";
-import Button from "../components/button";
-import { InputRoot, InputIcon, InputField } from "../components/input";
+'use client';
+
+import { User, Mail, ArrowRight } from 'lucide-react';
+// import { register } from "module";
+import Button from '../components/button';
+import { InputRoot, InputIcon, InputField } from '../components/input';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, 'Digite seu nome completo'),
+  email: z.string().email('Digite um e-mail válido'),
+});
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>;
 
 export default function SubscriptionForm() {
-    return (
-      <form
-        onSubmit={handleSubmit(onSubscribe)}
-        className="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]"
-      >
-        <h2 className="font-heading font-semibold text-gray-200 text-xl">
-          Inscrição
-        </h2>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubscriptionSchema>({
+    resolver: zodResolver(subscriptionSchema),
+  });
 
-        <div className="space-y-3">
-          <div className="space-y-2">
-            <InputRoot error={!!errors?.name}>
-              <InputIcon>
-                <User className="size-6" />
-              </InputIcon>
-              <InputField
-                type="text"
-                placeholder="Nome completo"
-                {...register('name')}
-              />
-            </InputRoot>
+  function onSubscribe(data: SubscriptionSchema) {
+    console.log(data);
+  }
+  return (
+    <form
+      onSubmit={handleSubmit(onSubscribe)}
+      className="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]"
+    >
+      <h2 className="font-heading font-semibold text-gray-200 text-xl">
+        Inscrição
+      </h2>
 
-            {errors?.name && (
+      <div className="space-y-3">
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <User className="size-6" />
+            </InputIcon>
+            <InputField
+              type="text"
+              placeholder="Nome completo"
+              {...register('name')}
+            />
+          </InputRoot>
+
+          {errors?.name && (
               <p className="text-danger font-semibold text-xs">
                 {errors.name.message}
               </p>
             )}
-          </div>
+        </div>
 
-          <div className="space-y-2">
-            <InputRoot error={!!errors?.email}>
-              <InputIcon>
-                <Mail className="size-6" />
-              </InputIcon>
-              <InputField
-                type="text"
-                placeholder="E-mail"
-                {...register('email')}
-              />
-            </InputRoot>
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <Mail className="size-6" />
+            </InputIcon>
+            <InputField
+              type="text"
+              placeholder="E-mail"
+              {...register('email')}
+            />
+          </InputRoot>
 
-            {errors?.email && (
+          {errors?.email && (
               <p className="text-danger font-semibold text-xs">
                 {errors.email.message}
               </p>
             )}
-          </div>
         </div>
+      </div>
 
-        <Button type="submit">
-          Confirmar
-          <ArrowRight className="size-6" />
-        </Button>
-      </form>
-    );
+      <Button type="submit">
+        Confirmar
+        <ArrowRight className="size-6" />
+      </Button>
+    </form>
+  );
 }
